35883ecd1ceef3e8198dbd7dc86d482298f62fc1
Dalton <dalton@MSI>
2024-04-06 12:46:20 -0600

    Fixed name convention problems

--- GcodeParser/Commands/HeaterTempCommand.cs
+++ GcodeParser/Commands/HeaterTempCommand.cs
@@ -1,14 +1,8 @@
 ﻿// Ignore Spelling: gcode
 
+using System.Text.RegularExpressions;
 using GCodeParser;
 using GCodeParser.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace GcodeParser.Commands
 {
@@ -21,10 +15,9 @@ namespace GcodeParser.Commands
     public sealed partial class HeaterTempCommand : Command
     {
 
-        private float temp;
-        private Heater heater;
-
-        private string command;
+        private readonly float _temp;
+        private readonly Heater _heater;
+        private readonly string _command;
 
         
 
@@ -35,8 +28,8 @@ namespace GcodeParser.Commands
         /// <param name="heater"></param>
         public HeaterTempCommand(float temp, Heater heater)
         {
-            this.temp = temp;
-            this.heater = heater;
+            _temp = temp;
+            _heater = heater;
             string commandStart = "";
             switch (heater)
             {
@@ -45,7 +38,7 @@ namespace GcodeParser.Commands
                 case Heater.hotend: commandStart = "M104"; break;
             }
 
-            command = $"{commandStart} S{temp} ";
+            _command = $"{commandStart} S{temp} ";
 
         }
 
@@ -60,25 +53,22 @@ namespace GcodeParser.Commands
             if (command.Contains("F") || command.Contains("B")) { throw new InvalidGCode($"Invalid HeaterTempCommand {command} - Does not support auto temp"); }
             if (command.Contains("I") || command.Contains("T")) { throw new InvalidGCode($"Invalid HeaterTempCommand {command} - Does not support multi index bed, hot end or materials"); }
 
-            this.command = command;
+            _command = command;
             if (Regex.IsMatch(command, "^M140"))
             {
-                heater = Heater.bed;
-                string SValue;
-                if (getStringAfterChar('S', command, out SValue)) temp = float.Parse(SValue);
+                _heater = Heater.bed;
+                if (GetStringAfterChar('S', command, out var sValue)) _temp = float.Parse(sValue);
 
             }
             else if (Regex.IsMatch(command, "^M104"))
             {
-                heater = Heater.hotend;
-                string SValue;
-                if (getStringAfterChar('S', command, out SValue)) temp = float.Parse(SValue);
+                _heater = Heater.hotend;
+                if (GetStringAfterChar('S', command, out var sValue)) _temp = float.Parse(sValue);
             }
             else if (Regex.IsMatch(command, "^M141"))
             {
-                heater = Heater.chamber;
-                string SValue;
-                if (getStringAfterChar('S', command, out SValue)) temp = float.Parse(SValue);
+                _heater = Heater.chamber;
+                if (GetStringAfterChar('S', command, out var sValue)) _temp = float.Parse(sValue);
             }
             else
             {
@@ -89,20 +79,27 @@ namespace GcodeParser.Commands
         /// <inheritdoc />
         public override string ToGCode(PrinterState state, GCodeFlavor gcodeFlavor)
         {
-            return AddInlineComment(this.command, gcodeFlavor);
+            return AddInlineComment(_command, gcodeFlavor);
         }
 
         /// <inheritdoc />
         protected override void ApplyToState(PrinterState state)
         {
-            switch (this.heater)
+            switch (_heater)
             {
-                case Heater.chamber: state.chamberTemp = temp; break;
-                case Heater.bed: state.bedTemp = temp; break ;
-                case Heater.hotend: state.hotEndTemp = temp; break;
+                case Heater.chamber: state.chamberTemp = _temp; break;
+                case Heater.bed: state.bedTemp = _temp; break ;
+                case Heater.hotend: state.hotEndTemp = _temp; break;
             }
         }
-        /// <inheritdoc />
+        
+        /// <summary>
+        /// Checks if the provided <paramref name="command"/> is a valid HeaterTempCommand.
+        /// </summary>
+        /// <param name="command">A single line of gcode, doesn't include any new line chars</param>
+        /// <param name="gcodeFlavor">Dictates the syntax to be used.</param>
+        /// <returns>True if the <paramref name="command"/> can be parsed as a HeaterTempCommand, False if otherwise.</returns>
+        /// <exception cref="InvalidGCode">Thrown if an unrecognized gcodeFlavor is provided</exception>
         public static bool IsCommand(string command, GCodeFlavor gcodeFlavor)
         {
             return gcodeFlavor switch
@@ -119,19 +116,19 @@ namespace GcodeParser.Commands
         /// <param name="command"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private bool getStringAfterChar(char charAfter, string command, out string value)
+        private static bool GetStringAfterChar(char charAfter, string command, out string value)
         {
             if (command.Contains(charAfter))
             {
                 int indexOfNum = command.IndexOf(charAfter) + 1;
 
-                int valueLength = command.IndexOf(" ", indexOfNum) - indexOfNum;
+                int valueLength = command.IndexOf(' ', indexOfNum) - indexOfNum;
                 if (valueLength < 0) valueLength = command.Length - indexOfNum;
 
                 value = command.Substring(indexOfNum, valueLength);
                 return true;
             }
-            value = "";
+            value = string.Empty;
             return false;
         }
 
