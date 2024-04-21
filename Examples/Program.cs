961132f7ace8010dfa19338694a983dd1f3f4097
Dalton <dalton@MSI>
2024-04-21 00:05:34 -0600

    Fixed Comments in Examples.Main

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -11,9 +11,7 @@ Stream debugStream = File.OpenWrite(gcodeFilePath);
 
 GCodeStreamWriter gcodeWriter = new(debugStream);
 
-// VaseMode.SpiralVase(gcodeWriter,0.2f, 0.8f, 1.75f, 25f, 75f, 3.502f, 5f, resolution: float.Pi/100, rotationsPerLayer: 2);
-
-// DotTexture.CircleOutline(gcodeWriter, 25, 30, 0.75f, 1.75f, offset: new(100, 100));
+VaseMode.SpiralVase(gcodeWriter,0.2f, 0.8f, 1.75f, 25f, 75f, 3.502f, 5f, resolution: float.Pi/100, rotationsPerLayer: 2);
 
 
 gcodeWriter.Flush();
